21111d58e7439bf1a3b5d6d1dd30a39ad8e46ff6
RavLyr <ravlybrb@gmail.com>
2023-08-13 21:02:26 +0700

    feat: ✨add section : contact,footer,about,projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,7 +32,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 /*=============== ADD BLUR TO HEADER ===============*/
 const scrollHeader = () =>{
     const header = document.getElementById('header')
-    // when the scroll s greater than 50 viewport height, add the scroll-header class to the header tag
+    // when the scroll s greater than 50 viewport height, add the blur-header class to the header tag
     this.scrollY >= 50 ? header.classList.add('blur-header')
                     : header.classList.remove('blur-header')
 }
@@ -40,12 +40,57 @@ const scrollHeader = () =>{
 window.addEventListener('scroll',blurHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+        contactMessage =  document.getElementById('contact-message')
 
 
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+ emailjs.sendForm('service_y81mzg9', 'template_4u18nue', '#contact-form', 'JCYlvV6Ej-llgKbPe')
+    .then(() => {
+        contactMessage.textContent = 'Your message has been sent successfully'
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 5000);
+
+        contactForm.reset()
+    },() => {
+        contactMessage.textContent = 'Something went wrong, please try again'
+       
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
